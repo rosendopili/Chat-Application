16a0e30397023f5e4cbd11b73c89251c7a44bbc2
rosendopili <rosendopili@gmail.com>
2020-02-28 13:31:22 -0500

    frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Chat from './components/Chat';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
